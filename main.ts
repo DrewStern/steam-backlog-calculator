a4ab543341ce43478be1d88c365cac4950e0e91d
Andrew <sternandrewm@gmail.com>
2020-07-31 22:27:59 -0500

    basically final version

--- main.ts
+++ main.ts
@@ -1,24 +1,7 @@
 import { HowLongToBeatService, HowLongToBeatEntry } from 'howlongtobeat';
 import * as fs from 'fs';
-import { write } from 'fs/promises';
-import { match } from 'assert';
-import { isArray, isNumber } from 'util';
-import { stringify } from 'querystring';
 
-// let hltb_service = new HowLongToBeatService();
-// let games: HowLongToBeatEntry[] = [];
-// for (let game_name of get_game_names()) {
-//     hltb_service.search(game_name).then(results => { 
-//         let matched_games: HowLongToBeatEntry[] = results.filter(x => x.similarity === 1);
-
-//         if (matched_games.length === 1) {
-//             // let best_match: HowLongToBeatEntry = matched_games[0];
-//             games.push(matched_games[0]);
-
-//             write_results(games);
-//         }
-//     });
-// }
+let NEWLINE = "\r\n";
 
 find_all_games(get_game_names())
 .then(games => write_results(games))
@@ -56,11 +39,11 @@ function is_exact_match(game: HowLongToBeatEntry): boolean {
 }
 
 function get_game_names(): string[] {
-    return fs.readFileSync('game_library.txt', 'utf8').split('\r\n');
+    return fs.readFileSync('game_library.txt', 'utf8').split(NEWLINE);
 }
 
 function write_results(games: Array<HowLongToBeatEntry>) {
-    // write_library_details(games);
+    write_library_details(games);
 
     let gameplayTiers = ["gameplayMain", "gameplayMainExtra", "gameplayCompletionist"];
     for (let gameplayTier of gameplayTiers) {
@@ -77,18 +60,21 @@ function write_library_details(games: Array<HowLongToBeatEntry>) {
 function write_library_playtime(games: Array<HowLongToBeatEntry>, gameplayTier: string) {
     let playtime_summer = (accumulator, current_game: HowLongToBeatEntry) => accumulator + current_game[gameplayTier];
     let total_playtime = games.reduce(playtime_summer, 0);
-    console.log("Total Playtime (@ '" + gameplayTier + "' tier): " + total_playtime);
-    // fs.appendFile('library_summary.txt', "Sum Main Playtime: " + sum_main_playtime, ()=>{});
+    let message = "Total Playtime (@ '" + gameplayTier + "' tier): " + total_playtime + NEWLINE;
+    // console.log(message);
+    fs.appendFile('library_summary.txt', message, ()=>{});
 }
 
 function write_shortest_playtime(games: Array<HowLongToBeatEntry>, gameplayTier: string) {
     games.sort((a, b) => a[gameplayTier] - b[gameplayTier]);
-    console.log("Shortest game (@ '" + gameplayTier + "' tier): " + games[0].name);
-    // fs.appendFile('library_summary.txt', "Sum Main Playtime: " + sum_main_playtime, ()=>{});
+    let message = "Shortest game (@ '" + gameplayTier + "' tier): " + games[0].name + NEWLINE;
+    // console.log(message);
+    fs.appendFile('library_summary.txt', message, ()=>{});
 }
 
 function write_longest_playtime(games: Array<HowLongToBeatEntry>, gameplayTier: string) {
     games.sort((a, b) => b[gameplayTier] - a[gameplayTier]);
-    console.log("Longest game (@ '" + gameplayTier + "' tier): " + games[0].name);
-    // fs.appendFile('library_summary.txt', "Sum Main Playtime: " + sum_main_playtime, ()=>{});
+    let message = "Longest game (@ '" + gameplayTier + "' tier): " + games[0].name + NEWLINE;
+    // console.log(message);
+    fs.appendFile('library_summary.txt', message, ()=>{});
 }

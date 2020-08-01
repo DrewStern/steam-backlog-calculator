99470bc66cc1dfb96703a48aac069aa15fafb6b4
Andrew <sternandrewm@gmail.com>
2020-07-31 23:59:42 -0500

    further work on the app - still not totally right

--- main.ts
+++ main.ts
@@ -14,13 +14,14 @@ function find_all_games(game_names: string[]): Promise<HowLongToBeatEntry[]> {
         promises.push(find_game(game_name)
             .then(matches => {
                 if (matches.length > 0) {
-                    let best_match: HowLongToBeatEntry = matches.filter(match => is_exact_match(match))[0];
+                    // console.log("matches = " + matches);
+                    console.log(matches[0]);
+                    let best_match: HowLongToBeatEntry = matches.filter(match => match.name.startsWith(game_name)).sort(compare_by_similarity)[0];
+                    // console.log("best_match = " + best_match);
                     if (!!best_match) {
-                        // console.log(best_match);
                         games.push(best_match);
                     }
                 }
-                // console.log("games.length = " + games.length);
             })
             .catch(error => 
                 console.log(error)
@@ -45,36 +46,48 @@ function get_game_names(): string[] {
 function write_results(games: Array<HowLongToBeatEntry>) {
     write_library_details(games);
 
-    let gameplayTiers = ["gameplayMain", "gameplayMainExtra", "gameplayCompletionist"];
+    let gameplayTiers = ["gameplayMain"/*, "gameplayMainExtra", "gameplayCompletionist"*/];
     for (let gameplayTier of gameplayTiers) {
         write_library_playtime(games, gameplayTier);
-        write_shortest_playtime(games, gameplayTier);
-        write_longest_playtime(games, gameplayTier);
+        write_by_playtime(games, gameplayTier);
     }
 }
 
 function write_library_details(games: Array<HowLongToBeatEntry>) {
-    fs.appendFile('library_details.txt', JSON.stringify(games), ()=>{});
+    appendFile('library_details.csv', "NAME,CASUAL_PLAYTIME,NORMAL_PLAYTIME,COMPLETE_PLAYTIME" + NEWLINE);
+    appendFile('library_details.csv', games.sort(compare_by_name).map(x => map_game_data_to_csv_format(x)).join(NEWLINE));
 }
 
 function write_library_playtime(games: Array<HowLongToBeatEntry>, gameplayTier: string) {
-    let playtime_summer = (accumulator, current_game: HowLongToBeatEntry) => accumulator + current_game[gameplayTier];
-    let total_playtime = games.reduce(playtime_summer, 0);
+    let playtime_accumulator = (accumulator, current_game: HowLongToBeatEntry) => accumulator + current_game[gameplayTier];
+    let total_playtime = games.reduce(playtime_accumulator, 0);
     let message = "Total Playtime (@ '" + gameplayTier + "' tier): " + total_playtime + NEWLINE;
-    // console.log(message);
-    fs.appendFile('library_summary.txt', message, ()=>{});
+    appendFile('library_summary.txt', message);
 }
 
-function write_shortest_playtime(games: Array<HowLongToBeatEntry>, gameplayTier: string) {
-    games.sort((a, b) => a[gameplayTier] - b[gameplayTier]);
-    let message = "Shortest game (@ '" + gameplayTier + "' tier): " + games[0].name + NEWLINE;
-    // console.log(message);
-    fs.appendFile('library_summary.txt', message, ()=>{});
+function write_by_playtime(games: Array<HowLongToBeatEntry>, gameplayTier: string) {
+    let message = "Shortest to Longest games (@ '" + gameplayTier + "' tier): " + NEWLINE + games.sort(compare_by_playtime).map(x => x.name + " (" + x[gameplayTier] + ")" + NEWLINE);
+    appendFile('library_summary.txt', message);
 }
 
-function write_longest_playtime(games: Array<HowLongToBeatEntry>, gameplayTier: string) {
-    games.sort((a, b) => b[gameplayTier] - a[gameplayTier]);
-    let message = "Longest game (@ '" + gameplayTier + "' tier): " + games[0].name + NEWLINE;
-    // console.log(message);
-    fs.appendFile('library_summary.txt', message, ()=>{});
+function map_game_data_to_csv_format(game: HowLongToBeatEntry): string {
+    return game.name + "," + game.gameplayMain + "," + game.gameplayMainExtra + "," + game.gameplayCompletionist;
+}
+
+function compare_by_name(game1: HowLongToBeatEntry, game2: HowLongToBeatEntry): number {
+    return 
+        game1.name == game2.name ? 0 :
+        game1.name > game2.name ? 1 : -1;
+}
+
+function compare_by_similarity(game1: HowLongToBeatEntry, game2: HowLongToBeatEntry): number {
+    return game1.similarity - game2.similarity;
+}
+
+function compare_by_playtime(game1: HowLongToBeatEntry, game2: HowLongToBeatEntry): number {
+    return game1.gameplayMain - game2.gameplayMain;
+}
+
+function appendFile(file_name: string, message: string) {
+    fs.appendFile(file_name, message, () => {});
 }
